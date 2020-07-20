d8b8c2c03a9ef05365b92aa7fcb7836f07fe00d6
Hamed Babagheybi <hbabagheybi@gmail.com>
2020-07-21 00:52:51 +0430

    brent, booth, brown fitness functions added.

--- Models/fitnessfunctions.py
+++ Models/fitnessfunctions.py
@@ -61,7 +61,7 @@ class FitnessFunctions:
 
         if len(value) == 2:
 
-            scores = (
+            score = (
                 (value[0] ** 2) +\
                     (2 * (value[1] ** 2)) -\
                     (0.3 * np.cos(3 *\
@@ -69,7 +69,7 @@ class FitnessFunctions:
                     (0.4 * np.cos(4 * np.pi * value[1])) + (0.7)
             )
         
-            return scores
+            return score
 
         else:
 
@@ -79,6 +79,94 @@ class FitnessFunctions:
     def bohachevskyn1_args(self):
 
         dimension, lower_bound, upper_bound = 2, -100, 100
+
+        result = {
+            'dim': dimension,
+            'lb': lower_bound,
+            'ub': upper_bound
+        }
+        
+        return result
+
+
+    def booth(self, value):
+        
+        if len(value) == 2:
+
+            score = (
+                ((value[0] +\
+                2 * value[1] - 7) ** 2) +\
+                ((2 * value[0] + value[1] - 5) ** 2)
+            )
+        
+            return score
+
+        else:
+
+            sys.exit('The Booth function is only defined on a 2D space.')
+
+
+    def booth_args(self):
+
+        dimension, lower_bound, upper_bound = 2, -10, 10
+
+        result = {
+            'dim': dimension,
+            'lb': lower_bound,
+            'ub': upper_bound
+        }
+        
+        return result
+
+    
+    def brent(self, value):
+
+        if len(value) == 2:
+
+            score = (
+                ((value[0] + 10) ** 2) +\
+                ((value[1] + 10) ** 2) +\
+                np.exp(-(value[0] ** 2 +\
+                value[1] ** 2))
+            )
+        
+            return score
+
+        else:
+
+            sys.exit('The Brent function is only defined on a 2D space.')
+
+    
+    def brent_args(self):
+
+        dimension, lower_bound, upper_bound = 2, -20, 0
+
+        result = {
+            'dim': dimension,
+            'lb': lower_bound,
+            'ub': upper_bound
+        }
+        
+        return result
+
+    
+    def brown(self, value):
+
+        score = 0
+
+        for i in range(len(value) - 1):
+
+            score += (
+                ((value[i] ** 2) ** ((value[i+1] ** 2) + 1)) +\
+                ((value[i+1] ** 2) ** ((value[i] ** 2) + 1))
+            )
+
+        return score
+
+    
+    def brown_args(self):
+
+        dimension, lower_bound, upper_bound = 0, -1, 4
 
         result = {
             'dim': dimension,
